89f235d81afc9952bcf555f6c3bb31aabe051aca
root <root@1.5b024a24a2.sandbox>
2026-01-29 09:29:00 +0000

    Draftbit Export -- auto commit

--- app.config.js
+++ app.config.js
@@ -30,12 +30,14 @@ module.exports = {
           "BeaTrackFam uses tracking to personalize your shopping experience and provide you with relevant product recommendations and exclusive offers.",
       },
     ],
+
     "expo-notifications",
     "expo-location",
     [
       "@stripe/stripe-react-native",
       { merchantIdentifier: "merchant.com.beatrackfaminc", enableGooglePay: true },
     ],
+
     [
       "expo-router",
       {
@@ -43,6 +45,7 @@ module.exports = {
         headOrigin: "https://5b024a24a2.sandbox.draftbit.dev:5100",
       },
     ],
+
     ["./plugins/draftbit-auto-launch-url-plugin"],
   ],
 
@@ -50,8 +53,15 @@ module.exports = {
   orientation: "portrait",
   icon: "https://v2-assets.draftbit.media/5b024a24a2/icon-2026-01-21T23:37:48.531Z.png",
   userInterfaceStyle: "automatic",
-  extra:
-    '{\n    EXPO_PUBLIC_SHOPIFY_DOMAIN: process.env.EXPO_PUBLIC_SHOPIFY_DOMAIN,\n    EXPO_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN:\n      process.env.EXPO_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,\n    EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY,\n    eas: {\n      projectId: "b7ca1c99-c6e5-4c54-be20-04bc3ee66994",\n    },\n  }',
+  extra: {
+    EXPO_PUBLIC_SHOPIFY_DOMAIN: process.env.EXPO_PUBLIC_SHOPIFY_DOMAIN,
+    EXPO_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN:
+      process.env.EXPO_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+    EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    eas: {
+      projectId: "b7ca1c99-c6e5-4c54-be20-04bc3ee66994",
+    },
+  },
 
   splash: {
     image: "./assets/appicon.png",
@@ -61,7 +71,7 @@ module.exports = {
   assetBundlePatterns: ["**/*"],
   ios: {
     supportsTablet: true,
-    buildNumber: "10",
+    buildNumber: "20",
     bundleIdentifier: "com.beatrackfaminc",
     usesAppleSignIn: true,
     requireFullScreen: true,
@@ -107,7 +117,7 @@ module.exports = {
       backgroundColor: "#ffffff",
     },
     package: "com.beatrackfaminc",
-    versionCode: 10,
+    versionCode: 20,
     permissions: [
       "INTERNET",
       "ACCESS_NETWORK_STATE",
